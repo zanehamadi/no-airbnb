ebfd7975b43cc6e78d81554cdf566aabed830a30
zaneh <zanehamadi@gmail.com>
2021-08-18 01:51:19 -0400

    deleted console logs

--- frontend/src/components/Search/index.js
+++ frontend/src/components/Search/index.js
@@ -20,13 +20,6 @@ export default function Search({solarSystems, planets, bookings}){
 
 
     useEffect(() => {
-        console.log('Temp:', temp);
-        console.log('System:', system);
-        console.log('Name:', name);
-        console.log('Booked', bookings[0])
-        console.log('Date:', date);
-        // console.log('startDate:', startDate)
-        // console.log('endDate:', endDate)
 
         let planetsArr = planets
 
@@ -34,43 +27,10 @@ export default function Search({solarSystems, planets, bookings}){
             planetsArr = planetsArr.filter(planet => ((planet.name).toUpperCase()).includes((name.toUpperCase())))
         }
 
-        // Yeah so basically I just remade startsWith() without knowing startsWith() existed 
-
-        // if(name){
-
-
-        //     planetsArr = planetsArr.filter(planet => {
-
-        //         let splitName = ((planet.name).toUpperCase()).split('')
-        //         let splitUserInput = (name.toUpperCase()).split('')
-        //         let userInputLength = splitUserInput.length
-        //         let checker = false
-        //         while(userInputLength){
-                    
-
-        //             if(!(splitName[userInputLength - 1] === splitUserInput[userInputLength - 1])){
-        //                 console.log('WE ARE HERE')
-        //                 break
-        //             }
-                    
-        //             userInputLength--
-        //             if(userInputLength === 0){
-        //                 checker = true
-        //             }
-        //         }
-
-
-
-        //         if(checker === true){
-        //             return planet
-        //         }
-        //     })
-        // }
 
         if(temp){
             planetsArr = planetsArr.filter(planet => +temp === +planet.temperature)
         }
-        console.log('Planets Arr:', planetsArr)
 
 
         if(system) {
@@ -79,93 +39,53 @@ export default function Search({solarSystems, planets, bookings}){
             planetsArr = planetsArr.filter(planet => solarId === planet.solar_system_id)
         }
 
-        // if(startDate && endDate){
-
-        //     let cantBook = bookings.filter(booking => {
-        //         console.log('Start Date Comparison - booking.startDate: ', booking.startDate)
-
-        //         let isBooked = false;
-        //         let badBookedArr = booking.startDate.split('-')
-        //         let badBookedEndArr = booking.endDate.split('-')
-        //         let bookedArr = []
-        //         let bookedEndArr = []
-        //         let cleanUp = 0;
-
-        //         let startDateInput = startDate.split('-')
-        //         let endDateInput = endDate.split('-')
-
-
-        //         while(cleanUp < 3){
         
-        //             if(cleanUp === 2){
-        //                 let badFormatDay = badBookedArr[cleanUp]
-        //                 let betterFormatDayLetters = badFormatDay.split('')
-        //                 let betterFormatDay = betterFormatDayLetters[0] + betterFormatDayLetters[1]
-
-        //                 let badFormatEndDay = badBookedEndArr[cleanUp]
-        //                 let betterFormatEndDayLetters = badFormatEndDay.split('')
-        //                 let betterFormatEndDay = betterFormatEndDayLetters[0] + betterFormatEndDayLetters[1]
-
-        //                 bookedArr.push(betterFormatDay)
-        //                 bookedEndArr.push(betterFormatDay)
-        //                 break
-        //             }
-
-        //             bookedEndArr.push(badBookedEndArr[cleanUp])
-        //             bookedArr.push(badBookedArr[cleanUp])
-        //             cleanUp++
-        //         }
-
-                
-        //         let bookedDate = bookedArr.join('-')
-        //         let bookedEndDate = bookedEndArr.join('-')
-
-        //         console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-
-        //         console.log('bookedDate:', bookedDate)
-        //         console.log('startDate:', startDate)
-
-        //         console.log('bookedEndDate:', bookedEndDate)
-        //         console.log('endDate:', endDate)
-
-        //         console.log('bookedArr:', bookedArr)
-        //         console.log('startDateInput:', startDateInput)
-
-        //         console.log('bookedEndArr:', bookedEndArr)
-        //         console.log('endDateInput:', endDateInput)
-
-        //         console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-
-
-
-
-        //     })
-
-        // }
-
         if(date){
             let startInputDate = date[0]
             let endInputDate = date[1]
 
-            bookings.filter(booking => endInputDate >= ((booking.startDate).toString()) && (startInputDate <= (booking.endDate)).toString())
-            bookings.forEach(booking => console.log( (booking.startDate).toString() ) )
+            bookings = bookings.filter(booking => {
+                
+
+                if((new Date(booking.startDate)) === startInputDate) return false
+                if((new Date(booking.endDate)) === endInputDate) return false
+
+                // desiredDate starts before bookedDate && desiredDate ends after bookDate starts
+                if((startInputDate <= (new Date(booking.startDate))) && (endInputDate >= (new Date(booking.endDate)))) return true;
+                // bookDate starts before desiredDate && desiredDate ends before bookDate
+
+                if(((new Date(booking.startDate)) <= startInputDate) && (endInputDate <= (new Date(booking.endDate)))) return true;
+
+                // bookDate starts before desiredDate && desiredDate starts before bookDate ends
+                if(((new Date(booking.startDate)) <= startInputDate) && (startInputDate <= (new Date(booking.endDate)))) return true;
+
+                // bookDate starts before desiredDate ends && desiredDate ends before bookDate ends
+                if(((new Date(booking.startDate)) <= endInputDate) && (endInputDate <= (new Date(booking.endDate)))) return true;
+
+                // desiredDate ends before bookedDate starts && bookedDate ends before desiredDate ends
+                if((endInputDate < (new Date(booking.startDate)))  && ((new Date(booking.endDate)) < endInputDate)) return true;
+                return false
+            })
+
+            planetsArr = planetsArr.filter(planet => {
+                let checker = true
+
+                bookings.forEach(booking => {
+                    if(+booking.planet_id === +planet.id){
+                        checker = false
+                    }
+                })
+                return checker
+            })
         }
         
-        console.log('Planets Arr2:', planetsArr)
 
+        console.log('AFTER BOOKINGS:', bookings)
+        console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
         setSearchPlanets(planetsArr)
 
-        console.log('AFTER - Search Planets:', searchPlanets)
 
     }, [temp, system, name, date])
-
-//     <div>
-//     {relevantPlanets ?                     
-//     <ul>
-//         {relevantPlanets.map(planet => <li><Link to={`/planets/${planet.id}`}>{planet.name}</Link></li>)}
-//     </ul> 
-//     : <></>}
-// </div>
 
     return(
         <form>
