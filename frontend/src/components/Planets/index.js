a234f32cd06d2ff9aaf58e3145e666c55b65b7c0
zaneh <zanehamadi@gmail.com>
2021-10-19 10:14:11 -0400

    changed planet styling

--- frontend/src/components/Planets/index.js
+++ frontend/src/components/Planets/index.js
@@ -8,13 +8,13 @@ export default function Planets({planets}){
 
     return(
         <div className="planetPageContainer">
-            <h2>Choose a Planet to visit</h2>
+            <h2 id="planetPageHeader">Choose a Planet to visit</h2>
             <div className="planetContainer">
                 {planets.map(planet => 
-                    <div key={planet.id}>
+                    <div key={planet.id} className="specPlanetDiv">
                         <Link to={`/planets/${planet.id}`} key={planet.id} className="planetsResults">
                         <img src={planet?.imgUrl}/> 
-                            {planet.name}
+                        <h3 id="planetPagePlanetName"> {planet.name} </h3>
                         </Link>
                     </div>)}
 
